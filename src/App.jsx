866a806612c1746c3f9f91f2ffee0b5e364334d9
Ojaswa Varshney <118374998+JrG-One@users.noreply.github.com>
2025-04-04 20:00:42 +0530

    Footer Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
 
 import { Button } from "@/components/ui/button"
+import HomePage from "./pages/Homepage"
 
 const App = ()=>{
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <HomePage/>
   )
 }
 
